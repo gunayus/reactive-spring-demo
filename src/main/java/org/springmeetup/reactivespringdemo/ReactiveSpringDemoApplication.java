b6f5ff298a83b4149ba3a499ecc2af205546e4fe
Erdem <erdem.gunay@turkcell.com.tr>
2018-07-12 08:38:00 +0300

    initiated project

---
+++ src/main/java/org/springmeetup/reactivespringdemo/ReactiveSpringDemoApplication.java
@@ -0,0 +1,12 @@
+package org.springmeetup.reactivespringdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveSpringDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveSpringDemoApplication.class, args);
+	}
+}
